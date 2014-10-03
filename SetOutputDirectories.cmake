7eb6d757a038950be5b47db6557af26f2fcc8cd2
Frank Park <piaoasd123@163.com>
2014-10-03 14:22:36 -0400

    Initial CMake scripts

---
+++ SetOutputDirectories.cmake
@@ -0,0 +1,12 @@
+SET( CMAKE_RUNTIME_OUTPUT_DIRECTORY ${CMAKE_SOURCE_DIR}/bin CACHE PATH "Single Directory for all executables." )
+SET( CMAKE_RUNTIME_OUTPUT_DIRECTORY_RELEASE ${CMAKE_SOURCE_DIR}/bin CACHE PATH "Single Directory for all executables." )
+SET( CMAKE_RUNTIME_OUTPUT_DIRECTORY_DEBUG ${CMAKE_SOURCE_DIR}/bin_debug CACHE PATH "Single Directory for all executables." )
+SET( CMAKE_ARCHIVE_OUTPUT_DIRECTORY ${CMAKE_SOURCE_DIR}/lib CACHE PATH "Single Directory for all static libraries." )
+SET( CMAKE_ARCHIVE_OUTPUT_DIRECTORY_RELEASE ${CMAKE_SOURCE_DIR}/lib CACHE PATH "Single Directory for all static libraries." )
+SET( CMAKE_ARCHIVE_OUTPUT_DIRECTORY_DEBUG ${CMAKE_SOURCE_DIR}/lib CACHE PATH "Single Directory for all static libraries." )
+SET( CMAKE_PDB_OUTPUT_DIRECTORY ${CMAKE_SOURCE_DIR}/bin_debug/pdb CACHE PATH "Single Directory for all static libraries." )
+SET( CMAKE_INCLUDE_CURRENT_DIR ON )
+
+if(WIN32)
+add_definitions( "-DPLATFORM_WINDOWS" )
+endif(WIN32)
